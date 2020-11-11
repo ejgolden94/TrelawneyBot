86c3a83c585a76194d449e37bb34d27a8bfa210f
Ellyn Golden <ejgolden94@gmail.com>
2020-11-11 11:40:58 -0500

    removing focus on button outline

--- messageGenerator.js
+++ messageGenerator.js
@@ -2,10 +2,10 @@
 // please add more words to the arrays :)
 let subjects = ['You','Your Mom', 'Your friends', 'Your family','Your life','The world','Everyone', 'No one','Your dog'];
 let adverbs = ['scantily','ignorantly','sadly','smartly','ideally','quirkily','shamelessly','forlournly','unfortunately','fortunately','ultimately','insanely','abhorrently','lovingly','amazingly','triumphantly'];
-let verbs = ['bask in','take your', 'sieze the', 'fail your','eat all of the','die by','run from the', 'smile at the','save the',
+let verbs = ['bask in','take your', 'sieze the', 'fail your','eat all of the','die by','run from the', 'smile at the','save the','piss on the',
             'hate your','love your','escape from the','leave the','climb the','ruin the','command the','ruin the','mourn the', 'gaze at the','be full of','fart on the','crush the','poop on the','become','reject the'];
 let objects = ['sunshine and rainbows','poop','sharks','candy','wealth','day','cake','bicycle','car','politics','plants','dogs','cats','love','life','horses','bisexuals',
-            'pickles','pottery','outer space','patriarchy','environment','vibes','capitalism','failure']
+            'pickles','pottery','outer space','patriarchy','environment','vibes','capitalism','failure','carpet']
 
 
 const getRandomSubject = () => {
