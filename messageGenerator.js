254a6d31354862c7b1c18b4141cc65baad1df2be
Ellyn Golden <ejgolden94@gmail.com>
2020-11-11 09:58:28 -0500

    more words

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,11 +1,11 @@
 //// This is a Fortune Telling Bot 
 // please add more words to the arrays :)
 let subjects = ['You','Your Mom', 'Your friends', 'Your family','Your life'];
-let adverbs = ['scantily','ignorantly','sadly','smartly','ideally','quirkily','shamelessly','forlournly'];
+let adverbs = ['scantily','ignorantly','sadly','smartly','ideally','quirkily','shamelessly','forlournly','unfortunately','fortunately'];
 let verbs = ['bask in','take your', 'sieze the', 'fail your','eat all of the','die by','run from the', 'smile at the',
-            'hate your','love your','escape from','leave the','climb the','ruin the','command the', 'gaze at the','be full of'];
+            'hate your','love your','escape from','leave the','climb the','ruin the','command the', 'gaze at the','be full of','fart on','crush the'];
 let objects = ['sunshine and rainbows','poop','sharks','candy','wealth','day','cake','bicycle','car','White House','plants','dogs','cats','love','life','loss','horses',
-            'pickles','pottery','outer space']
+            'pickles','pottery','outer space','patriarchy']
 
 
 const getRandomSubject = () => {
